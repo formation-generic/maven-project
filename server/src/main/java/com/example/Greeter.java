6061d53c2fc71d9732aa0d461550a432cf25c338
formation-generic <65594475+formation-generic@users.noreply.github.com>
2020-05-20 10:05:09 +0200

    Update Greeter.java
    Correction faite sur la JavaDoc et sur Final

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
- //TODO: Add javadoc comment
-  public String greet(String someone) {
+ /**
+   * @Param someone  : the name of a person
+   * @Return greting string
+   */
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
